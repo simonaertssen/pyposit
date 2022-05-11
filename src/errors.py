329df2678645fe8bb9799ca076cae20162c6b385
simonaertssen <simonaertssen@gmail.com>
2022-05-11 17:03:51 +0200

    Necessary files for a good python production

---
+++ src/errors.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+class BitSizeError(BaseException):
+    """The wrong bit size was given."""
+
+
+class NotSupportedError(BaseException):
+    """Indicate that a certain type is not supported."""
