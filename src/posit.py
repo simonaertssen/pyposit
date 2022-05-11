b0f3c711629026733718a740f5bef2e2f74dbff2
simonaertssen <simonaertssen@gmail.com>
2022-05-11 17:03:04 +0200

    A good initialiser

--- src/posit.py
+++ src/posit.py
@@ -1,48 +1,88 @@
+# -*- coding: utf-8 -*-
 from math import log2, ceil
-from sys import getsizeof
+
+from src.errors import NotSupportedError, BitSizeError
 
 
 class Posit(object):
     """Represents a posit number, a drop-in replacement for float."""
 
-    def __init__(self, bits: str = '0', nbits: int = 16, es: int = 1) -> None:
-        """Initialise the number and cast to desired format."""
-        # Verify and add remaining leading 0s
-        print(len(bits), nbits)
-        assert(len(bits) <= nbits)
-        bits = '0'*(nbits - len(bits)) + bits
-        assert(len(bits) == nbits)
+    def __init__(self, bits: str, numbits: int = 16, es: int = 1) -> None:
+        """Initialise the number and extract the different posit pieces."""
+        # Verify input
+        if not isinstance(bits, str):
+            raise NotSupportedError(f"{type(bits)} is not supported.")
 
-        self.nbits: int = nbits
+        if numbits not in (8, 16, 32, 64):
+            raise BitSizeError()
+
+        if bits.startswith("0b"):
+            bits.removeprefix("0b")
+
+        bitlen: int = len(bits)
+        if bitlen > numbits:
+            raise ValueError(f"{bits} contains {len(bits)} > {numbits}")
+
+        # Fixed values
+        self.bits: str = bits
+        self.numbits: int = numbits
         self.es: int = es
         self.useed: int = 2**(2**self.es)
 
-        # Calculate indices of different parts
-        regimelen: int = int(bits[1::], 2).bit_length()
-        for i in (0, 1, 1 + regimelen, 1 + regimelen + es)
-        self.bits: tuple[int] =
+        # Preallocate for switching between states
+        self.sign: int = 0
+        self.regime: int = 0
+        self.exponent: int = 0
+        self.fraction: int = 0
 
-        self.sign: int = int(bits[0], 2)
-        self.regime: int = int(bits[1:regimelen], 2)
+        self.k: int = 0
+        self.signlen: int = 1
+        self.regimelen: int = 0
+        self.fraclen: int = 0
 
-        self.exponent: int = int(bits[regimelen:regimelen+es], 2)
-        self.fraction: int = int(bits[regimelen+es::], 2)
+        # Now register the correct values for sign, regime, exponent and fraction
+        bits = '0' * (numbits - bitlen) + bits
+        self.sign: int = int(bits[0], base=2)
 
-        self.num_regime_bits: int = len(self.regime)
-        self.num_exponent_bits: int = len(self.exponent)
-        self.num_fraction_bits: int = len(self.fraction)
+        # Start of the regime is where bits differ
+        firstbit: int = int(bits[1], base=2)
 
-        self.k: int = self._run_length_encoding(self.regime)
+        self.regimelen = 1  # + 1 for sign offset
+        try:  # whether any change occurs in the entire bitarray
+            # print(str(1 - firstbit))
+            # print(bits[1:].index(str(1 - firstbit)))
+            self.regimelen += bits[1:].index(str(1 - firstbit))
+        except ValueError:
+            self.regimelen = numbits - 1
+        else:
+            self.regimelen += 1  # + 1 including the index in the range
+        assert 1 < self.regimelen < numbits
 
-    def _run_length_encoding(self, regime: int) -> int:
-        """Turn the regime into a run-length encoded number k."""
-        # First find leading 1s and 0s: https://skalkoto.blogspot.com/2008/01/bit-operations-find-first-zero-bit.html
-        first_one: int = regime.bit_length()
-        popcount: int = bin(regime).count("1")
-        k = first_one - self.num_regime_bits
-        if popcount > 1:
-            k += popcount - 1
-        return k
+        self.regime = int(bits[1:self.regimelen], base=2)
+        self.k = firstbit * (self.regimelen - 3) - (1 - firstbit) * (self.regimelen - 2)
+
+        # Set in case of extremes: 0 or infinity
+        if bits == '0' * numbits:
+            self.sign = 0
+            return
+        elif bits == '1' + '0' * (numbits - 1):
+            self.sign = 1
+            return
+
+        self.exponent = int(bits[self.regimelen:self.regimelen + es], base=2)
+        self.fraction = int(bits[self.regimelen + es:], base=2)
+        self.fraclen = numbits - self.signlen - self.regimelen - es
+        assert numbits == self.signlen + self.regimelen + self.es + self.fraclen
+
+    # def _run_length_encoding(self, regime: int, regimelen: int) -> int:
+    #     """Turn the regime into a run-length encoded number k."""
+    #     # First find leading 1s and 0s: https://skalkoto.blogspot.com/2008/01/bit-operations-find-first-zero-bit.html
+    #     first_one: int = regime.bit_length()
+    #     popcount: int = bin(regime).count("1")
+    #     k: int = first_one - regimelen
+    #     if popcount > 1:
+    #         k += popcount - 1
+    #     return k
 
     def __repr__(self) -> str:
         return f"posit({self.to_float()})"
@@ -50,11 +90,11 @@ class Posit(object):
     def to_float(self) -> float:
         s: int = (-1)**(self.sign)
         r: int = self.useed**(self.k)
-        e: int = self.base**(self.exponent)
+        e: int = 2**(self.exponent)
 
         denum: int = 2**(ceil(log2(self.fraction + 1)))
         f: float = self.fraction / denum
-        print(s, e, r, f)
+        # print(s, e, r, f)
         return s * r * e * (1 + f)
 
     def __add__(self, other: 'Posit') -> 'Posit':
@@ -64,12 +104,5 @@ class Posit(object):
 if __name__ == "__main__":
     p1 = Posit(bits='0000110111011101', es=3)
     print(p1)
-
-    # p2 = Posit()
-    # print(p2)
-
-    # p3 = p1 + p2
-    # print(p3)
-
-    number = 3
-    print(number.bit_length())
+    # p1 = Posit(bits='00001000', es=3)
+    # print(p1)
--- tests/test_posit.py
+++ tests/test_posit.py
@@ -1,43 +1,41 @@
 # -*- coding: utf-8 -*-
 import unittest
-from posit import Posit
+from src.posit import Posit
 
 
 class PositTester(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.p: Posit = Posit()
+        self.p: Posit = Posit(bits='00000000', numbits=8)
 
     def test_initialization(self) -> None:
         """Test whether the posit was initialised well."""
-        self.p.__init__(sign='0', regime='0', exponent='0', fraction='0')
+        self.p.__init__(bits='00000000', numbits=8)
         self.assertEqual(self.p.sign, 0)
         self.assertEqual(self.p.regime, 0)
         self.assertEqual(self.p.exponent, 0)
         self.assertEqual(self.p.fraction, 0)
-        self.assertEqual(self.p.num_regime_bits, 1)
-        self.assertEqual(self.p.num_exponent_bits, 1)
-        self.assertEqual(self.p.num_fraction_bits, 1)
 
-        self.p.__init__(sign='0', regime='0001', exponent='101', fraction='11011101')
-        self.assertEqual(self.p.sign, 0)
-        self.assertEqual(self.p.regime, 1)
-        self.assertEqual(self.p.exponent, 5)
-        self.assertEqual(self.p.fraction, 221)
-        self.assertEqual(self.p.num_regime_bits, 4)
-        self.assertEqual(self.p.num_exponent_bits, 3)
-        self.assertEqual(self.p.num_fraction_bits, 8)
+        self.p.__init__(bits='10000000', numbits=8)
+        self.assertEqual(self.p.sign, 1)
+        self.assertEqual(self.p.regime, 0)
+        self.assertEqual(self.p.exponent, 0)
+        self.assertEqual(self.p.fraction, 0)
+
+        self.p.__init__('0000110111011101', numbits=16, es=3)
+        self.assertEqual(self.p.sign, int('0', base=2))
+        self.assertEqual(self.p.regime, int('0001', base=2))
+        self.assertEqual(self.p.exponent, int('101', base=2))
+        self.assertEqual(self.p.fraction, int('11011101', base=2))
 
     def test_run_length_encoding(self) -> None:
         """Test the correctness of the ruin length encoding of k."""
-        self.p.__init__(regime='1000')  # Initialise with a correct regime length
-
-        regimes: list[int] = [0b0000, 0b0001, 0b0010, 0b0100, 0b1000, 0b1100, 0b1110, 0b1111]
-        expected_k: list[int] = [-4, -3, -2, -1, 0, 1, 2, 3]
+        regimes: list[int] = ['0001', '0010', '0100', '1000', '1100', '1110']
+        expected_k: list[int] = [-3, -2, -1, 0, 1, 2]
 
         for regime, k in zip(regimes, expected_k):
-            # self.p._run_length_encoding(regime)
-            self.assertEqual(self.p._run_length_encoding(regime), k)
+            self.p.__init__(bits='0' + regime + '101', numbits=8, es=1)
+            self.assertEqual(self.p.k, k)
 
     def test_addition(self) -> None:
         """Test whether the posit addition is correct."""
